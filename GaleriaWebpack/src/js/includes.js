ecec30db0dd2930c3fe1a6d98d1271a17d6f9569
Efraim-Santos <efraim.bezerra@souunit.com.br>
2019-08-15 07:02:54 -0300

    TestandoJS

--- GaleriaWebpack/src/js/includes.js
+++ GaleriaWebpack/src/js/includes.js
@@ -1,21 +1,30 @@
-export function onLoadHtmmlSucess(callback) {
+const loadHtmlSucessCallbacks = []
+
+export function onLoadHtmlSucess(callback) {
     if(!loadHtmlSucessCallbacks.includes(callback)){
         loadHtmlSucessCallbacks.push(callback)
     }
 }
 
 function loadIncludes(parent){
-    if(!parent) parent = 'body'
     let elemento = document.querySelectorAll('[include]')
-   
-    elemento.forEach(e => {
-        const url = e.getAttribute('include')
-        fetch(url)
-            .then(resp => resp.text())
-            .then(html => {
-                e.innerHTML = html
-            })
-    })
+
+    console.log(elemento)
+    if(elemento.length != 0){
+        elemento.forEach(e => {
+            const url = e.getAttribute('include')
+            fetch(url)
+                .then(resp => resp.text())
+                .then(html => {
+                    e.innerHTML = html
+                    e.removeAttribute('include')
+                    // loadHtmlSucessCallbacks.forEach(
+                    //     callback => callback()
+                    // )
+                    loadIncludes(e)
+                })
+        })
+    }
 }
 
 loadIncludes()
