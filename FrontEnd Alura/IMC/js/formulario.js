60b1817c478a9bb28b3527082eb3f7ab24e7ce02
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2020-12-07 00:15:32 -0300

    parteLogica

--- FrontEnd Alura/IMC/js/formulario.js
+++ FrontEnd Alura/IMC/js/formulario.js
@@ -1,2 +1,46 @@
-var adicionar = document.querySelector('#add')
-// adicionar.a
+var adicionar = document.querySelector("#add")
+
+function criarTd() {
+    return document.createElement("td")
+}
+
+function getInformacoes(){
+    let nome = document.querySelector("#nome").value
+    let peso = document.querySelector("#peso").value
+    let altura = document.querySelector("#altura").value
+    let gordura = document.querySelector("#gordura").value
+
+    return criarElemento(nome, peso, altura, gordura)
+}
+function criarElemento(nome, peso, altura, gordura){
+    let linha = document.createElement("tr")
+    let linhaNome = document.createElement("td")
+    let linhaPeso = document.createElement("td")
+    let linhaAltura = document.createElement("td")
+    let linhaGordura = document.createElement("td")
+    let linhaImc = document.createElement("td")
+
+    linhaNome.textContent = nome
+    linhaPeso.textContent = peso
+    linhaAltura.textContent = altura
+    linhaGordura.textContent = gordura
+    linhaImc.textContent = peso / (altura * altura)
+
+    linha.appendChild(linhaNome)
+    linha.appendChild(linhaPeso)
+    linha.appendChild(linhaAltura)
+    linha.appendChild(linhaGordura)
+    linha.appendChild(linhaImc)
+
+    return linha
+}
+adicionar.addEventListener("click", (event)=>{
+    
+    event.preventDefault()
+    
+    let pacientes = document.querySelector("#tabela-pacientes")
+    
+    pacientes.appendChild(getInformacoes())
+
+
+})
