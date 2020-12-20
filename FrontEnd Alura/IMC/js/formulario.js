2371acc32440eb1d4af6b7d6aa34a19ac0de844b
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2020-12-19 21:50:26 -0300

    regras

--- FrontEnd Alura/IMC/js/formulario.js
+++ FrontEnd Alura/IMC/js/formulario.js
@@ -1,31 +1,33 @@
 var adicionar = document.querySelector("#add")
 
-function criarTd() {
-    return document.createElement("td")
-}
-
-function validarInfo(nome, peso, altura, gordura){
-
-    let validador = document.querySelector("#validador")
+adicionar.addEventListener("click", (event)=>{
     
-    if(nome && peso && altura && gordura){
+    event.preventDefault()
+    
+    let pacientes = document.querySelector("#tabela-pacientes")
+    var validador = document.querySelector("#validador")
+    let form = document.querySelector("form")
 
-        if(peso <= 0 || peso > 300){
-            validador.style.display = "block"
-            validador.textContent = "Peso Inválido!"
-        }else if(altura < 0.5 || altura >= 3){
-            validador.style.display = "block"
-            validador.textContent = "Altura Inválida!"
-        } else if (gordura <= 0){
-            validador.style.display = "block"
-            validador.textContent = "Peso Invalido"
-        }else{
+    getInformacoes() ? pacientes.appendChild(getInformacoes()) : false
+    
+    form.reset();
+})
+//Valida se existe paciente com os mesmos dados
+function validarRepedtido(nome, peso, altura, gordura){
+    let nomes = document.querySelectorAll(".info-nome")
+    let pesos = document.querySelectorAll(".info-peso")
+    let alturas = document.querySelectorAll(".info-altura")
+    let gorduras = document.querySelectorAll(".info-gordura")
+
+    for (let i = 0; i < nomes.length; i++) {
+        if((nomes[i].textContent == nome) && (pesos[i].textContent == peso) && (alturas[i].textContent == altura) && (gorduras[i].textContent == gordura)){
+            var aux = []
+            aux.push("Paciente já foi adicionado!")
+            imprimirErros(aux, validador)
             return true
         }
-    }else{
-        validador.style.display = "block"
-        validador.textContent = "Campos não preenchidos"
     }
+    return false
 }
 
 function getInformacoes(){
@@ -37,26 +39,101 @@ function getInformacoes(){
    let validou = validarInfo(nome, peso, altura, gordura)
 
    if(validarInfo(nome, peso, altura, gordura)){
-      return criarElemento(nome, peso, altura, gordura)
+        if (!(validarRepedtido(nome, peso, altura, gordura))){
+            validador.style.display = "none"
+            return criarElemento(nome, peso, altura, gordura)
+        }
    }else {
        return false
    }
 }
+//Validar informações passadas no formulario
+function validarInfo(nome, peso, altura, gordura){
 
+    var erros = []
+    
+    if(nome && peso && altura && gordura){
+        
+        let pesoEhValido, alturaEhValida, gorduraEhValida = false
 
+        validaPeso(peso) ? pesoEhValido = true : erros.push("Peso é Inválido!")
+        validaAltura(altura) ? alturaEhValida = true : erros.push("Altura é inválida!")
+        validaPeso(peso) ? gorduraEhValida = true : erros.push("Gordura é Inválida!")
+
+        if (pesoEhValido && alturaEhValida && gorduraEhValida) {
+            return true
+        }else {
+            imprimirErros(erros, validador)
+
+            return false
+        }
+    }else{
+        nome ? nome : erros.push("O nome não pode ser em branco!")
+        peso ? peso : erros.push("O peso não pode ser em branco!")
+        altura ? altura : erros.push("A altura não pode ser em branco!")
+        gordura ? gordura : erros.push("A gordura não pode ser em branco!")
+        
+        imprimirErros(erros, validador)
+        
+        return false
+    }
+}
+
+function imprimirErros(erros, tagHtml){
+    let imprimir = ""
+    validador.style.display = "block"
+    erros.forEach((valor) => {
+        imprimir += `${valor} <br>`
+        tagHtml.innerHTML = imprimir
+    })    
+}
+
+function criarTd() {
+    return document.createElement("td")
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validarGordura(gordura){
+    if (gordura >= 0) {
+        return true
+    }else {
+        return false
+    }
+}
+
+//Crio os elementos html e já insiro o valor do IMC
 function criarElemento(nome, peso, altura, gordura){
     let linha = document.createElement("tr")
     let linhaNome = document.createElement("td")
+    linhaNome.classList.add("info-nome")
     let linhaPeso = document.createElement("td")
+    linhaPeso.classList.add('info-peso')
     let linhaAltura = document.createElement("td")
+    linhaAltura.classList.add('info-altura')
     let linhaGordura = document.createElement("td")
+    linhaGordura.classList.add('info-gordura')
     let linhaImc = document.createElement("td")
 
     linhaNome.textContent = nome
     linhaPeso.textContent = peso
     linhaAltura.textContent = altura
     linhaGordura.textContent = gordura
-    linhaImc.textContent =  (peso / (altura * altura)).toFixed(2)
+    linhaImc.textContent =  (peso / (altura * altura)).toFixed(2) // Calcula IMC e já insere no html
 
     linha.appendChild(linhaNome)
     linha.appendChild(linhaPeso)
@@ -66,12 +143,3 @@ function criarElemento(nome, peso, altura, gordura){
 
     return linha
 }
-adicionar.addEventListener("click", (event)=>{
-    
-    event.preventDefault()
-    
-    let pacientes = document.querySelector("#tabela-pacientes")
-
-    getInformacoes ? pacientes.appendChild(getInformacoes()) : getInformacoes
-
-})
