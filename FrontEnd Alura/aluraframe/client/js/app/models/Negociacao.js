db13b080c91efe51caa3a7f64c04bfb998d884ab
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-13 07:25:52 -0300

    prosseguimos

---
+++ FrontEnd Alura/aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,26 @@
+class Negociacao {
+    
+    constructor (data, quantidade, valor){
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        // this._volume = this._volume;
+        Object.freeze(this);
+    };
+    
+    get data(){
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade(){
+        return this._quantidade
+    }
+
+    get valor(){
+        return this._valor
+    }
+
+    get calcularVolume(){
+        return this._quantidade * this._valor
+    }
+};
