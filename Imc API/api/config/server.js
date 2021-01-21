564c434bfc0fad50229eb8fee9c368940ed1b425
Efraim-Santos <efraim.santos@avonale.com.br>
2021-01-21 13:59:55 -0300

    api

--- Imc API/api/config/server.js
+++ Imc API/api/config/server.js
@@ -1,38 +1,10 @@
 const express = require('express');
 const app = express();
-const data = require("../data/pacientes.json");
+const rotas = require('../controllers/rotas')
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json(data);
-});
+rotas(app);
 
 app.listen(3000)
 
-// app.get("/pacientes/:id", (req, res)=>{
-//     const { id } = req.params;
-//     const pacientes = data.find(valor => valor.id == id);
-
-//     if(!pacientes) return res.status(404).json();
-    
-//     res.json(pacientes);
-// });
-
-
-// app.post("/pacientes", (req, res) => {
-//     const {nome, peso, altura} = req.body;
-  
-//     res.json({nome, peso, altura});
-// });
-
-
-
-// app.put("/pacientes/:id", (req, res) => {});
-
-// app.delete("/pacientes/:id", (req, res) => {
-//     const { id } = req.params;
-//     const clientsFiltered = data.filter(paciente => paciente.id != id);
-
-//     res.json(clientsFiltered);
-// });
