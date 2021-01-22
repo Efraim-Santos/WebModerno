c331d903f5894b57ff1ede5b6379b25fc1598055
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-22 00:16:02 -0300

    api

--- Imc API/api/server.js
+++ Imc API/api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const rotas = require('../controllers/rotas')
+const rotas = require('./controllers/rotas')
 
 app.use(express.json());
 
