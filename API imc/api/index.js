968da4297d7e4a93b92eaeb194e3ef334186774d
EFRAIM-SANTOS <EFRAIM.BEZERRA@SOUUNIT.COM.BR>
2021-01-22 02:15:00 -0300

    api

--- API imc/api/index.js
+++ API imc/api/index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
 const pacientes = require('./data/pacientes')
-module.exports = (req, res) => {
+
+app.get('/', (req, res)=>{
     res.json(pacientes)
-  }
+});
+
+// module.exports = (req, res) => {
+//     res.json(pacientes)
+//   }
